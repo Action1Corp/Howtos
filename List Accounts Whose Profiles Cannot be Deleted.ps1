ca77ed28113d1475da581116c6225812b8cd182d
Aleksy Rembicki <Aleksy.Rembicki.c@action1.com>
2026-05-26 11:17:22 +0200

    Update legal notices and headers.  (#1)
    Add legal notices
    Add Readme.md
    Add legal headers
    Remove obsolete files
    Update List All Missing Windows Updates function.

--- List Accounts Whose Profiles Cannot be Deleted.ps1
+++ List Accounts Whose Profiles Cannot be Deleted.ps1
@@ -1,5 +1,12 @@
 # List of accounts whose profiles cannot be deleted
 
+# Action1 Public Repository Material
+# Subject to TERMS_OF_USE.md (https://github.com/Action1Corp/PSAction1/blob/main/TERMS_OF_USE.md)
+# Provided AS IS
+# Use at your own risk
+# Review and test before production deployment
+# © Action1 Corporation
+
 $ExcludedUsers = ”Public”, ”zenoss”, ”svc”, ”user_1”, ”user_2”
 
 $LocalProfiles = Get-WMIObject -class Win32_UserProfile | Where { (!$_.Special) -and (!$_.Loaded) -and ($_.ConvertToDateTime($_.LastUseTime) -lt (Get-Date).AddDays(-60)) }
--- List Installed Software on Multiple Computers.ps1
+++ List Installed Software on Multiple Computers.ps1
@@ -1,3 +1,10 @@
+# Action1 Public Repository Material
+# Subject to TERMS_OF_USE.md (https://github.com/Action1Corp/PSAction1/blob/main/TERMS_OF_USE.md)
+# Provided AS IS
+# Use at your own risk
+# Review and test before production deployment
+# © Action1 Corporation
+
 #computers from a text file: 
 Get-Content -Path c:\computers.txt | ForEach-Object { Get-WmiObject -Namespace ROOT\CIMV2 -Class Win32_Product -Computer $_ }
 
